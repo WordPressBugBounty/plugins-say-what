f3823b425ca69e74fb5da2a1ab407a1dca1294c8
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 01:55:13 +0000

    2.2.4

---
+++ say-what/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dfe5c208f23cac0f6e6838011299d3dfa36d9670',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dfe5c208f23cac0f6e6838011299d3dfa36d9670',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
